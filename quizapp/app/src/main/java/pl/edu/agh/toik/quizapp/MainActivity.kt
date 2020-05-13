ff1ef659be26bb3730b7e09c51e52efaa3ec0302
Michał Śledź <sledzson08@gmail.com>
2020-05-13 16:23:26 +0200

    Add swagger codegen

--- quizapp/app/src/main/java/pl/edu/agh/toik/quizapp/MainActivity.kt
+++ quizapp/app/src/main/java/pl/edu/agh/toik/quizapp/MainActivity.kt
@@ -2,11 +2,13 @@ package pl.edu.agh.toik.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import pl.edu.agh.toik.quizapp.model.Question
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Question()
     }
 }
