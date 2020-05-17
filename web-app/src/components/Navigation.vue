7a218516e37574b39e8ca782d34b2c0355243b03
Bartłomiej Stępień <bartek.bartlomiej.96@gmail.com>
2020-05-17 02:37:42 +0200

    Make some adjustments of "Navigation" component

--- web-app/src/components/Navigation.vue
+++ web-app/src/components/Navigation.vue
@@ -1,43 +1,42 @@
 <template>
-  <b-navbar>
-    <template slot="brand">
-      <b-navbar-item tag="router-link" :to="{ path: '/' }">
-        <img
-          src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
-          alt="Lightweight UI components for Vue.js based on Bulma"
-        >
-      </b-navbar-item>
-    </template>
-    <template slot="start">
-      <b-navbar-item tag="router-link" :to="{ path: '/' }">
-        Home
-      </b-navbar-item>
-      <b-navbar-item href="#">
-        Documentation
-      </b-navbar-item>
-      <b-navbar-dropdown label="Info">
-        <b-navbar-item tag="router-link" :to="{ path: '/About' }">
-          About
+    <b-navbar
+      fixed-top
+      shadow
+      spaced
+      wrapper-class="container is-widescreen"
+    >
+      <template slot="brand">
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          <img src="favicon.ico" alt="Quiz! Logo">
         </b-navbar-item>
-        <b-navbar-item href="#">
-          Contact
-        </b-navbar-item>
-      </b-navbar-dropdown>
-    </template>
+      </template>
 
-    <template slot="end">
-      <b-navbar-item tag="div">
-        <div class="buttons">
-          <a class="button is-primary">
-            <strong>Sign up</strong>
-          </a>
-          <a class="button is-light">
-            Log in
-          </a>
-        </div>
-      </b-navbar-item>
-    </template>
-  </b-navbar>
+      <template slot="start">
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          Start Quiz!
+        </b-navbar-item>
+      </template>
+
+      <template slot="end">
+        <b-navbar-item tag="router-link" :to="{ path: '/About'}" >
+          <span class="icon">
+            <b-icon pack="fa" icon="edit" size="is-small" />
+          </span>
+          <span>Manage data</span>
+        </b-navbar-item>
+        <b-navbar-dropdown label="Info">
+          <b-navbar-item
+            href="https://github.com/bartek-bartlomiej/agh-toik-quiz-app"
+            target="_blank"
+          >
+            <span class="icon">
+              <b-icon pack="fab" icon="github" size="is-small"/>
+            </span>
+            <span>Github</span>
+          </b-navbar-item>
+        </b-navbar-dropdown>
+      </template>
+    </b-navbar>
 </template>
 
 <script>
