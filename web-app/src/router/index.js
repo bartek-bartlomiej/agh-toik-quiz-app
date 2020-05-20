7c70029b0a45585dc0065cb7fefa097dc0cb0434
Bartłomiej Stępień <bartek.bartlomiej.96@gmail.com>
2020-05-20 18:53:04 +0200

    Add the "Quiz" view draft

--- web-app/src/router/index.js
+++ web-app/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Quiz from '../views/Quiz'
 
 Vue.use(VueRouter)
 
@@ -9,6 +10,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/quiz',
+    name: 'Quiz',
+    component: Quiz
   },
   {
     path: '/about',
---
+++ web-app/src/views/Quiz.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="section">
+    <div class="container is-widescreen">
+      <div class="columns is-centered">
+        <div class="column is-two-thirds-tablet is-half-desktop">
+          <nav class="level">
+            <div class="level-item">
+              <h1 class="title is-4 is-spaced">Question 1 ouf of 10</h1>
+            </div>
+          </nav>
+          <section class="section">
+            <h2 class="subtitle has-text-centered">Who is the most powerful hero in the world?</h2>
+            <div class="buttons">
+              <template v-for="(answer, index) in ['Thanos', 'Shaggy', 'Superman', 'Saitama']">
+                <b-button
+                  :key="index"
+                  expanded
+                  outlined
+                  size="is-large"
+                  type="is-primary">{{ answer }}
+                </b-button>
+              </template>
+            </div>
+            <nav class="level">
+              <div class="level-left"/>
+              <div class="level-right">
+                <div class="level-item">
+                  <b-button
+                    disabled
+                    type="is-primary">
+                    Next
+                  </b-button>
+                </div>
+              </div>
+            </nav>
+          </section>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Quiz'
+}
+</script>
