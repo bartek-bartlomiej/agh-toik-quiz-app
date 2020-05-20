369a2992f3e20340b4c23124272f39686246b533
Bartłomiej Stępień <bartek.bartlomiej.96@gmail.com>
2020-05-20 20:50:13 +0200

    Add logic for single question

---
+++ web-app/src/components/quiz/AnswerButton.vue
@@ -0,0 +1,41 @@
+<template>
+  <b-button
+    :class="{ 'is-inactive': isQuestionAnswered }"
+    :outlined="!isQuestionAnswered ? true : !selected"
+    :type="!isQuestionAnswered ? 'is-primary' : (isCorrectAnswer ? 'is-success' : (selected ? 'is-danger' : 'is-light'))"
+    @click="select"
+    expanded
+    size="is-large">
+    {{ text }}
+  </b-button>
+</template>
+
+<script>
+export default {
+  name: 'answer-button',
+  data: function () {
+    return {
+      selected: false
+    }
+  },
+  props: {
+    text: String,
+    index: Number,
+    isQuestionAnswered: Boolean,
+    isCorrectAnswer: Boolean
+  },
+  methods: {
+    select () {
+      this.$el.blur()
+      this.$emit('answer-selected', this.index)
+      this.selected = true
+    }
+  }
+}
+</script>
+
+<style>
+  .is-inactive {
+    pointer-events: none;
+  }
+</style>
--- web-app/src/views/Quiz.vue
+++ web-app/src/views/Quiz.vue
@@ -5,20 +5,20 @@
         <div class="column is-two-thirds-tablet is-half-desktop">
           <nav class="level">
             <div class="level-item">
-              <h1 class="title is-4 is-spaced">Question 1 ouf of 10</h1>
+              <h1 class="title is-4 is-spaced">Question {{ currentQuestionIndex + 1 }} ouf of {{ questions.length }}</h1>
             </div>
           </nav>
           <section class="section">
-            <h2 class="subtitle has-text-centered">Who is the most powerful hero in the world?</h2>
+            <h2 class="subtitle has-text-centered">{{ currentQuestion.body }}</h2>
             <div class="buttons">
-              <template v-for="(answer, index) in ['Thanos', 'Shaggy', 'Superman', 'Saitama']">
-                <b-button
+              <template v-for="(answer, index) in currentQuestion.answers">
+                <answer-button
+                  :index="index"
+                  :is-correct-answer="index === currentQuestion.correctAnswer"
+                  :is-question-answered="isCurrentQuestionAnswered"
                   :key="index"
-                  expanded
-                  outlined
-                  size="is-large"
-                  type="is-primary">{{ answer }}
-                </b-button>
+                  :text="answer"
+                  @answer-selected="saveAnswer"/>
               </template>
             </div>
             <nav class="level">
@@ -26,7 +26,7 @@
               <div class="level-right">
                 <div class="level-item">
                   <b-button
-                    disabled
+                    :disabled="!isCurrentQuestionAnswered"
                     type="is-primary">
                     Next
                   </b-button>
@@ -41,7 +41,39 @@
 </template>
 
 <script>
+import AnswerButton from '../components/quiz/AnswerButton'
+
 export default {
-  name: 'Quiz'
+  name: 'Quiz',
+  components: { AnswerButton },
+  data: function () {
+    return {
+      questions: [
+        {
+          answers: ['Thanos', 'Shaggy', 'Superman', 'Saitama'],
+          body: 'Who is the most powerful hero in the world?',
+          category: 'movies',
+          correctAnswer: 1,
+          difficulty: 'hard',
+          id: 0
+        }
+      ],
+      answers: [],
+      currentQuestionIndex: 0
+    }
+  },
+  computed: {
+    currentQuestion () {
+      return this.questions[this.currentQuestionIndex]
+    },
+    isCurrentQuestionAnswered () {
+      return this.answers.length === this.currentQuestionIndex + 1
+    }
+  },
+  methods: {
+    saveAnswer (answerIndex) {
+      this.answers.push(answerIndex)
+    }
+  }
 }
 </script>
