b8a2e1ed1bfc7e5a51fa09646332481376d45c63
Bartłomiej Stępień <bartek.bartlomiej.96@gmail.com>
2020-05-21 04:46:25 +0200

    Adjust the "Home" view

--- web-app/src/views/Home.vue
+++ web-app/src/views/Home.vue
@@ -1,24 +1,76 @@
 <template>
-  <div>
-    <section class="hero is-medium is-white is-bold">
-      <div class="hero-body">
-        <figure class="image center">
-          <img id="logo" src="../assets/logo.svg" alt="Quiz! logo">
-        </figure>
+  <section class="hero is-fullheight is-white is-bold">
+    <div class="hero-head">
+      <navigation/>
+    </div>
+    <div class="hero-body">
+      <div class="container is-widescreen">
+        <div class="columns is-centered">
+          <div class="column is-two-thirds">
+            <div class="content">
+              <figure class="image is-centered">
+                <img id="logo" src="../assets/logo.svg" alt="Quiz! logo">
+              </figure>
+
+              <form>
+                <section class="section box">
+                  <b-field horizontal label="Category" custom-class="is-large">
+                    <b-select placeholder="Select a category" size="is-large">
+                      <option
+                        v-for="(option, index) in [1,2,3]"
+                        :value="option"
+                        :key="index">
+                        {{ option }}
+                      </option>
+                    </b-select>
+                  </b-field>
+
+                  <b-field horizontal label="Difficulty" custom-class="is-large">
+                    <div class="buttons has-addons">
+                      <b-button size="is-large">Easy</b-button>
+                      <b-button size="is-large" type="is-primary" class="is-selected">Medium</b-button>
+                      <b-button size="is-large">Hard</b-button>
+                    </div>
+                  </b-field>
+
+                  <b-field horizontal label="Quantity" custom-class="is-large">
+                    <b-numberinput
+                      min="0"
+                      max="100"
+                      icon-pack="fa"
+                      controls-position="compact"
+                      size="is-large" />
+                  </b-field>
+                </section>
+                <div class="buttons is-centered">
+                  <b-button type="is-primary" size="is-large">Start Quiz!</b-button>
+                  <b-button type="is-white">Manage questions</b-button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
-    </section>
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    </div>
+  </section>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Navigation from '@/components/Navigation'
+
+// Difficulty:
+//   type: string
+// enum:
+// - hard
+// - medium
+// - easy
+// default: medium
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    Navigation
   }
 }
 </script>
