4d46a336d7bdd821a66967545a5addc88ee74674
bartek-bartlomiej <35294021+bartek-bartlomiej@users.noreply.github.com>
2020-05-21 17:20:29 +0200

    "Navigation" bar tweaks

--- web-app/src/components/Navigation.vue
+++ web-app/src/components/Navigation.vue
@@ -10,13 +10,10 @@
         </b-navbar-item>
       </template>
 
-      <template slot="start">
-        <b-navbar-item tag="router-link" :to="{ path: '/' }">
-          Start Quiz!
-        </b-navbar-item>
-      </template>
-
       <template slot="end">
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          Home
+        </b-navbar-item>
         <b-navbar-item tag="router-link" :to="{ path: '/About'}" >
           <span class="icon">
             <b-icon pack="fa" icon="edit" size="is-small" />
